6d9fd7b15cde3d8b7368947a2207be65444be5c8
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-08-15 17:51:16 -0700

    Added schooting direction adjestments

--- bundle.js
+++ bundle.js
@@ -64,14 +64,36 @@
 
 	function Asteroid (options){
 	  this.COLOR = "grey";
-	  this.RADIUS = 20;
+	  this.RADIUS = 20 + Math.random() * 20;
 	  this.points = [];
 	  MovingObject.call(this, {pos: options.pos, color: this.COLOR, radius: this.RADIUS, vel: randomVec(3)});
+	  this.createPoints();
 	}
 	inherits(Asteroid, MovingObject);
 
 	Asteroid.prototype.createPoints = function () {
+	  for(let i = 0; i < 360; i += 60) {
+	    let dist = this.radius / 2 + Math.random() * this.radius / 2;
+	    let radians = i / 180 * Math.PI;
+	    let x = Math.cos(radians) * dist;
+	    let y = Math.sin(radians) * dist;
+	    this.points.push([x,y]);
+	  }
+	};
 
+	Asteroid.prototype.draw = function(ctx) {
+	  ctx.fillStyle = this.color;
+	  ctx.beginPath();
+	  let start = this.points[this.points.length - 1];
+	  let startX = this.pos[0] + start[0];
+	  let startY = this.pos[1] + start[1];
+	  ctx.moveTo(startX, startY);
+	  this.points.forEach((point) => {
+	    let x = this.pos[0] + point[0];
+	    let y = this.pos[1] + point[1];
+	    ctx.lineTo(x, y);
+	  });
+	  ctx.fill();
 	};
 
 	module.exports = Asteroid;
@@ -334,26 +356,62 @@
 	  this.vel = [0,0];
 	  this.COLOR = "white";
 	  this.RADIUS = 10;
-	  this.facingDir = [0,-1];
+	  this.facingDir = 0;
 	  this.game = options.game;
 	  MovingObject.call(this, {pos: options.pos, color: this.COLOR, radius: this.RADIUS, vel: this.vel});
 
 	}
 
+	Array.prototype.equals = function (array) {
+	    // if the other array is a falsy value, return
+	    if (!array)
+	        return false;
+
+	    // compare lengths - can save a lot of time
+	    if (this.length !== array.length)
+	        return false;
+
+	    for (var i = 0, l=this.length; i < l; i++) {
+	        // Check if we have nested arrays
+	        if (this[i] instanceof Array && array[i] instanceof Array) {
+	            // recurse into the nested arrays
+	            if (!this[i].equals(array[i]))
+	                return false;
+	        }
+	        else if (this[i] !== array[i]) {
+	            return false;
+	        }
+	    }
+	    return true;
+	  };
+	// Hide method from for-in loops
+	Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+
 	inherits(Ship, MovingObject);
 
 	Ship.prototype.power = function (impulse) {
-	  this.facingDir = impulse;
+	  console.log(impulse);
+	  if (impulse.equals([-1, 0]) && this.facingDir !== Math.PI) {
+	    this.facingDir <= Math.PI ? this.facingDir += 30 / 180 * Math.PI : this.facingDir -= 30 / 180 * Math.PI;
+	  }
+	  if (impulse.equals([1, 0]) && this.facingDir !== 0) {
+	    this.facingDir <= 0 ? this.facingDir += 30 / 180 * Math.PI : this.facingDir -= 30 / 180 * Math.PI;
+	  }
+	  if (impulse.equals([0, 1]) && this.facingDir !== (Math.PI / 2 * 3)) {
+	    this.facingDir <= (3 * Math.PI / 2) ? this.facingDir += 30 / 180 * Math.PI : this.facingDir -= 30 / 180 * Math.PI;
+	  }
+	  if (impulse.equals([0, -1]) && this.facingDir !== (Math.PI / 2)) {
+	    this.facingDir <= (Math.PI / 2) ? this.facingDir += 30 / 180 * Math.PI : this.facingDir -= 30 / 180 * Math.PI;
+	  }
+	  console.log(this.facingDir);
 	  this.vel[0] += impulse[0];
 	  this.vel[1] += impulse[1];
 	};
 
 
 	Ship.prototype.fireBullet = function () {
-	  let pos = [this.pos[0] + (this.radius * this.facingDir[0]), this.pos[1] + (this.radius * this.facingDir[1])]
-	  let vel = [this.vel[0] + (5 * this.facingDir[0]), this.vel[1] + (5 * this.facingDir[1])];
-	  console.log(this.vel);
-	  console.log(vel);
+	  let pos = [this.pos[0] + (this.radius * Math.cos(this.facingDir)), this.pos[1] + (this.radius * Math.sin(this.facingDir))];
+	  let vel = [this.vel[0] + (5 * Math.cos(this.facingDir)), this.vel[1] + (5 * Math.sin(this.facingDir))];
 	  let bullet = new Bullet({pos: pos, vel: vel});
 	  this.game.bullets.push(bullet);
 	};
